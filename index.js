7862894198c3c3b0d8024286b51b790ec15dab08
jhersbo <jhersbo@gmail.com>
2021-12-30 15:59:12 -0600

    color change functionality working. just need to add timer and trial repeat functionality

--- index.js
+++ index.js
@@ -1,32 +1,63 @@
 let colorDiv = document.getElementById('color')
 let startButton = document.getElementById('start')
+let finished = document.getElementById('finished-indicator')
+let timer = document.getElementById('timer')
 
-function update(){
-    let globalCount = 0
-    let color = 1
-    //1 = green 0 = red
-    if (globalCount === 9){
-        $('color').hide()
-        return
-    }
-    if(color === 1){
-        colorDiv.style.backgroundColor = 'green'
-        color = 0; 
-        globalCount++
-        wait(5000).then(()=>{
-            update()
-            console.log('update 5000')
-        })
-    }
-    if(color === 0){
-        colorDiv.style.backgroundColor = 'red'
-        wait(3000).then(()=>{
-            update()
-            console.log('update 3000')
-        })
-        color = 1
+//Stopwatch
+
+let sec = 0;
+let stopTime = false
+
+function cycle(){
+    if(stopTime === false){
+        console.log('Input time:', seconds, 'Actual time:', sec)
+        sec += 1;
+        timer.textContent = sec
+        setTimeout(cycle, 1000)
     }
 }
+
+function stopTimer(){
+    if (stopTime === false){
+        stopTime = true
+    }
+}
+
+let color = 1;
+let count = 0;
+//this function is responsible for switching the colors and text for each trial
+function update(){
+    if(count < 9){
+        if(color === 1){
+            colorDiv.style.backgroundColor = 'green'
+            // globalCount++
+            wait(5000).then(()=>{
+                color--;
+                count++
+                update()
+            })
+        }
+        if(color === 0){
+            colorDiv.style.backgroundColor = 'red'
+            wait(3000).then(()=>{
+                color++;
+                count++
+                update()
+            })
+        }
+    }
+    if(count === 9){
+        colorDiv.style.backgroundColor = 'white'
+        finished.textContent = 'Finished!'
+    }
+}
+
+//this function strings together the multiple trials, seperated by 30 second breaks
+// let loop = 0;
+
+// function loop(){
+
+// }
 
 function wait(ms){
     return new Promise(resolve => setTimeout(resolve, ms));
