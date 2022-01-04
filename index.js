22044b7620fca5863b94d1a22f8d56cae4ffbb8c
jhersbo <jhersbo@gmail.com>
2022-01-04 16:58:44 -0600

    finished and functioning. added readme

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ let colorDiv = document.getElementById('color')
 let startButton = document.getElementById('start')
 let finished = document.getElementById('finished-indicator')
 let timer = document.getElementById('timer')
+let boxLabel = document.getElementById('indicator')
+let trialIndicator = document.getElementById('trial-indicator')
 
 //Stopwatch
 function time(seconds){
@@ -18,8 +20,11 @@ function time(seconds){
 
 let color = 1;
 let count = 0;
+let globalCount = 0;
 //this function is responsible for switching the colors and text for each trial
 function update(){
+    boxLabel.textContent = 'Color Indicator'
+    trialIndicator.textContent = `Trial ${globalCount + 1}`
     if(count < 9){
         if(color === 1){
             time(5)
@@ -41,17 +46,55 @@ function update(){
         }
     }
     if(count === 9){
-        colorDiv.style.backgroundColor = 'white'
+        globalCount++
+        colorDiv.style.backgroundColor = '#f8f9fa'
         timer.textContent = ''
         finished.textContent = 'Finished!'
+        wait(1000).then(()=>{
+            color = 1
+            count = 0
+            finished.textContent = ''
+            subjectBreak()
+            wait(31000).then(()=>{
+                if(globalCount <= 7){
+                    update()
+                }else{
+                    boxLabel.textContent = ''
+                    timer.textContent = ''
+                    trialIndicator.textContent = ''
+                    finished.textContent = 'Full Test Complete! Please refresh the page.'
+                    return
+                }
+            })
+        })
     }
+}
+
+function subjectBreak(){
+    boxLabel.textContent = 'Break Time!'
+    time(30)
+    return
+}
+
+function darker(){
+    startButton.style.backgroundColor = '#69b578'
+}
+
+function lighter(){
+    startButton.style.backgroundColor = '#3a7d44'
 }
 
 function wait(ms){
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-startButton.addEventListener('click', (e)=>{
+startButton.addEventListener('mousedown', (e)=>{
     e.preventDefault()
+    darker()
     update()
+})
+
+startButton.addEventListener('mouseup', (e)=>{
+    e.preventDefault()
+    lighter()
 })
